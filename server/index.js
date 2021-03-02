923e98e6521f3b690878a8e677b1a0b41b95b4e6
chandanapriya23 <chandanapriya708@gmail.com>
2021-03-01 22:57:30 -0600

    import express

--- server/index.js
+++ server/index.js
@@ -1,4 +1,5 @@
-const app = require('express')();
+const express = require('express');
+const app = express();
 const http = require('http').createServer(app);
 const path = require('path');
 const io = require('socket.io')(http);
